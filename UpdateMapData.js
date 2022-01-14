703fadd89de2f7fbcf2385961831e129447b1d71
Sklyvan <joangraciataribo@gmail.com>
2022-01-15 00:10:08 +0100

    Avoid doing any work if the locations list is empty.

--- UpdateMapData.js
+++ UpdateMapData.js
@@ -9,20 +9,24 @@ export function updateMapData()
 
     const InformationGPS = document.getElementById('serverData').value;
 
-    const datetime = InformationGPS.map(function (item) { return [item[0], item[1]]; });
-    const latlngs = InformationGPS.map(function (item) { return [parseFloat(item[2]), parseFloat(item[3])]; });
-    const speed = InformationGPS.map(function (item) { return parseFloat(item[4]); });
-
-    const initialDate = convertToDate(datetime[0][0]); const initialTime = convertToTime(datetime[0][1]);
-    const finalDate = convertToDate(datetime[latlngs.length - 1][0]); const finalTime = convertToTime(datetime[latlngs.length - 1][1]);
-
-    L.marker(latlngs[0]).addTo(MainMap).bindPopup('Inicio: ' + convertToTimezone(initialDate, initialTime)).openPopup();
-    L.marker(latlngs[latlngs.length - 1]).addTo(MainMap).bindPopup('Final: ' + convertToTimezone(finalDate, finalTime));
-
-    if (latlngs.length > 0)
+    if (InformationGPS.length != 0)
     {
-        const polyline = L.polyline(latlngs, {color: 'RED', opacity: 0.75, weight: 3}).addTo(MainMap);
-        MainMap.fitBounds(polyline.getBounds());
+        const datetime = InformationGPS.map(function (item) { return [item[0], item[1]]; });
+        const latlngs = InformationGPS.map(function (item) { return [parseFloat(item[2]), parseFloat(item[3])]; });
+        const speed = InformationGPS.map(function (item) { return parseFloat(item[4]); });
+
+        const initialDate = convertToDate(datetime[0][0]); const initialTime = convertToTime(datetime[0][1]);
+        const finalDate = convertToDate(datetime[latlngs.length - 1][0]); const finalTime = convertToTime(datetime[latlngs.length - 1][1]);
+
+        L.marker(latlngs[0]).addTo(MainMap).bindPopup('Inicio: ' + convertToTimezone(initialDate, initialTime)).openPopup();
+        L.marker(latlngs[latlngs.length - 1]).addTo(MainMap).bindPopup('Final: ' + convertToTimezone(finalDate, finalTime));
+
+        if (latlngs.length > 0)
+        {
+            const polyline = L.polyline(latlngs, {color: 'RED', opacity: 0.75, weight: 3}).addTo(MainMap);
+            MainMap.fitBounds(polyline.getBounds());
+        }
     }
+
     console.log("Mapa Actualizado!");
 }
