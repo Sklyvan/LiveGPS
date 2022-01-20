327ebce971e6e89a2658521ad1bfe3b514d4e6d5
Sklyvan <joangraciataribo@gmail.com>
2022-01-20 22:47:19 +0100

    Fixed zoom bug.

--- updateMap.js
+++ updateMap.js
@@ -1,5 +1,5 @@
 var markers; var polyline;
-
+var fitMap = true;
 function updateMapData()
 {
     const InformationGPS = document.getElementById('serverData').value;
@@ -9,7 +9,6 @@ function updateMapData()
         const datetime = InformationGPS.map(function (item) { return [item[0], item[1]]; });
         const latlngs = InformationGPS.map(function (item) { return [parseFloat(item[2]), parseFloat(item[3])]; });
         const speed = InformationGPS.map(function (item) { return parseFloat(item[4]); });
-
         const initialDate = convertToDate(datetime[0][0]); const initialTime = convertToTime(datetime[0][1]);
         const finalDate = convertToDate(datetime[latlngs.length - 1][0]); const finalTime = convertToTime(datetime[latlngs.length - 1][1]);
 
@@ -18,9 +17,14 @@ function updateMapData()
         let finalMarker = L.marker(latlngs[latlngs.length - 1]).addTo(MainMap);
         finalMarker.bindPopup('Final: ' + convertToLocalTimezone(finalDate, finalTime));
         markers = [initialMaker, finalMarker];
-
-        polyline = L.polyline(latlngs, {color: 'RED', opacity: 0.75, weight: 3}).addTo(MainMap);
-        MainMap.fitBounds(polyline.getBounds());
+        // For every element inside latlngs, create a marker
+        for (let i = 0; i < latlngs.length-1; i++)
+        {
+            let x1 = latlngs[i]; let x2 = latlngs[i + 1];
+            let polyline = L.polyline([x1, x2], { color: 'RED', opacity: 0.75 }).addTo(MainMap);
+        }
+        polyline = L.polyline(latlngs);
+        if (fitMap) MainMap.fitBounds(polyline.getBounds());
     }
 }
 
