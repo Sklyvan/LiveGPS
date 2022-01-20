28efcc90f5c4c0c3b14732db1b145b48082c8766
Sklyvan <joangraciataribo@gmail.com>
2022-01-20 21:28:03 +0100

    Fixed bug when the date is in the correct format.

--- convertionFunctions.js
+++ convertionFunctions.js
@@ -80,19 +80,29 @@ function convertToUTCTimezone(date, time)
 function convertToServerDate(inputDate)
 {
     // Convert date in format YYYY-MM-DD to YYMMDD
-    let date = inputDate.split('-');
-    let YY = date[0].substring(2, 4);
-    let MM = date[1];
-    let DD = date[2];
-    return YY + MM + DD;
+
+    if (inputDate.indexOf('-') > -1)
+    {
+        let date = inputDate.split('-');
+        let YY = date[0].substring(2, 4);
+        let MM = date[1];
+        let DD = date[2];
+        return YY + MM + DD;
+    }
+    return inputDate;
+
 }
 
 function convertToServerTime(inputTime)
 {
     // Convert time in format HH:MM:SS to HHMMSS
-    let time = inputTime.split(':');
-    let HH = time[0];
-    let MM = time[1];
-    let SS = time[2];
-    return HH + MM + SS;
+    if (inputTime.indexOf(':') > -1)
+    {
+        let time = inputTime.split(':');
+        let HH = time[0];
+        let MM = time[1];
+        let SS = time[2];
+        return HH + MM + SS;
+    }
+    return inputTime;
 }
