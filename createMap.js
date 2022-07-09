861185d4936fbcefa75dfd7d030fa5f11641a606
Sklyvan <joangraciataribo@gmail.com>
2022-07-09 20:24:28 +0200

    Created the home location icon.
    Took 1 minute

--- createMap.js
+++ createMap.js
@@ -1,5 +1,17 @@
-const MainMap = L.map('Map').setView([41.3414506,2.1635851], 13);
+const MainMap = L.map('Map').setView([41.3414506,2.1635851]);
 const secretKey = document.getElementById('KEY').innerHTML;
+const homeLocation = L.latLng(0,0);
+// Add a house icon at the home location
+let homeMarker = L.marker(homeLocation).addTo(MainMap);
+// Change the icon of the home marker
+homeMarker.setIcon(L.icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+}));
+
 
 L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key='+secretKey,
     {attribution: '<a href="https://github.com/Sklyvan" title="Copyright">Sklyvan</a>'}).addTo(MainMap);
